07bd22903ba314d91a05d3508d2fdee88d2b7818
Julia Helena <contato.juliahsantana@gmail.com>
2024-11-25 19:42:01 -0300

    feat: add new section on inicio

--- src/Pages/Inicio/Inicio.jsx
+++ src/Pages/Inicio/Inicio.jsx
@@ -1,4 +1,8 @@
 import s from './inicio.module.scss'
+import Oferece from '../../assets/oferece.png'
+import estimula from '../../assets/estimula.png'
+import fornece from '../../assets/fornece.png'
+import garante from '../../assets/garante.png'
 
 export default function Inicio(){
     return(
@@ -7,9 +11,27 @@ export default function Inicio(){
             <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
         </section>
 
-        <section>
-            <h3>Por que devo doar?</h3>
-        </section>
-    </main>
+  <section className={s.porqueDoarSection}>
+                <h2>Por que devo doar?</h2>
+                <div className={s.containerCards}>
+                    <section>
+                        <img src={Oferece} alt="Imagem ilustrando círculo social de 4 pessoas" />
+                        <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+                    </section>
+                    <section>
+                        <img src={estimula} alt="Imagem ilustrando pessoa lendo" />
+                        <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+                    </section>
+                    <section>
+                        <img src={fornece} alt="Imagem ilustrando 6 pessoas transformando vidas" />
+                        <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+                    </section>
+                    <section>
+                        <img src={garante} alt="Imagem ilustrando balança que representa igualdade" />
+                        <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+                    </section>
+                </div>
+            </section>
+        </main>
     )
 }
