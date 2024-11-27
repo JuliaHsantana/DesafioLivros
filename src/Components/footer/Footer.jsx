36fd6742b4b0ac8d248d0edcca8d43c4765f9463
Julia Helena <contato.juliahsantana@gmail.com>
2024-11-27 19:52:38 -0300

    feat: finalize footer and create QueroDoar page

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,9 +1,27 @@
 import s from './footer.module.scss'
+import facebook from '../../assets/face.png'
+import youtube from '../../assets/youtube.png'
+import instagram from '../../assets/insta.png'
+import tt from '../../assets/tt.png'
+import linkedln from '../../assets/linkedln.png'
 
 export default function Footer(){
     return(
         <footer className={s.footer}>
-            <h2>EU SOU A FOOTER</h2>
+        <section className={s.contatosFooter}>
+        <p>4002-8922</p>
+
+        <nav>
+            <a href="https://www.facebook.com/login/" target='_blank'>  <img src={facebook} alt="logo facebook" /> </a>
+            <a href="https://x.com/" target='_blank'> <img src={tt} alt="logo twitter" /> </a>
+            <a href="https://www.youtube.com/"target='_blank'> <img src={youtube} alt="logo youtube" /> </a>
+            <a href="https://br.linkedin.com/"target='_blank'> <img src={linkedln} alt="logo linkedln" /> </a>
+            <a href="https://www.instagram.com/"target='_blank'> <img src={instagram} alt="logo instagram" /> </a>
+        </nav>
+        </section>
+        <section className={s.copyrigth}>
+     <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024  </p>
+        </section>
         </footer>
     )
 }
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,7 +1,22 @@
+import livro from '../../assets/livross.png'
+import s from './queroDoar.module.scss'
+
 export default function QueroDoar(){
     return(
-        <section>
-            <h2>queroDoar</h2>
+        <section className={s.queroDoarSection}>
+           <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
+
+         <form action="">
+            <div>
+          <img src={livro} alt="icone de um livro aberto" />
+          <p>Informações do Livro</p>
+            </div>
+            <input type="text" name="" id=""  placeholder='Titulo'/>
+             <input type="text" name="" id=""  placeholder='Categoria'/>
+              <input type="text" name="" id="" placeholder='Autor'/>
+               <input type="text" name="" id="" placeholder='Link da Imagem' />
+               <input type="submit" value="Doar" className={s.buttonDoar}/>
+         </form>
         </section>
     )
 }
