be1019aae33de917fd651f095d81f3616b237e68
Julia Helena <contato.juliahsantana@gmail.com>
2024-11-29 19:35:00 -0300

    feat: add page livros Doados

--- src/Pages/LivrosDoados/LivrosDoados.jsx
+++ src/Pages/LivrosDoados/LivrosDoados.jsx
@@ -1,7 +1,21 @@
+import protagonista from '../../assets/Protagonista.png'
+import s from './livroDoados.module.scss'
+
+
 export default function LivrosDoados(){
     return(
-        <section>
-            <h2>livros</h2>
+        <section className={s.livrosDoadosSection}>
+           <h2>Livros Doados</h2>
+           <section className={s.containerCard}>
+               <section>
+                 <img src={protagonista} alt="capa do livro O protagonista vermelha" />
+                 <div>
+                 <h3>O Protagonista</h3>
+                 <p>Susanne Andrade</p>
+                 <p>ficção</p>
+                 </div>
+               </section>
+           </section>
         </section>
     )
 }
